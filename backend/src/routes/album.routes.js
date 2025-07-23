b9ddafab8593f7cb641ba608608eb1e898f32b66
SamamaKarim092 <samamakarim90@gmail.com>
2025-07-23 12:36:09 +0500

    3rd section complete

--- backend/src/routes/album.routes.js
+++ backend/src/routes/album.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { getAllAlbums, getAlbumById } from "../controller/album.controller.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Album route with get method');
-});
+router.get('/', getAllAlbums);
+router.get('/:albumsId', getAlbumById);
+
 
 export default router;
