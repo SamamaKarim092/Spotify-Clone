3d4b3c933d6d69f3491289ad95a263a0ae8014a5
SamamaKarim092 <samamakarim90@gmail.com>
2025-07-27 11:03:33 +0500

    Correction in the front-end with some making the UI

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,47 +1,40 @@
-import { SignedIn, SignedOut, SignOutButton, UserButton } from "@clerk/clerk-react";
+import { SignedOut, UserButton } from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButtons from "./SignInOAuthButtons";
 import { useAuthStore } from "@/stores/useAuthStore";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
 
+const Topbar = () => {
+	const { isAdmin } = useAuthStore();
+	console.log({ isAdmin });
 
-const TopBar = () => {
-  const {isAdmin} = useAuthStore();
-  console.log({isAdmin})
-  return (
-    <div className="flex item-center justify-between p-4 sticky top-0 bg-zinc-900/75 
-    backdrop-blur-md z-10">
-      
-      <div className="flex gap-2 items-center"> 
-        <img src="./spotify.png" className="size-8" alt="spotify logo" />
-        Spotify 
-      </div>
-          
-    <div className="flex items-center gap-4">
-      {isAdmin && (
-        <Link to ="/admin">
-          <LayoutDashboardIcon className="size-4 mr-2" />
-          Admin Dashboard
-        </Link>
-      )}
+	return (
+		<div
+			className='flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 
+      backdrop-blur-md z-10
+    '
+		>
+			<div className='flex gap-2 items-center'>
+				<img src='/spotify.png' className='size-8' alt='Spotify logo' />
+				Spotify
+			</div>
+			<div className='flex items-center gap-4'>
+				{isAdmin && (
+					<Link to={"/admin"} className={cn(buttonVariants({ variant: "outline" }))}>
+						<LayoutDashboardIcon className='size-4  mr-2' />
+						Admin Dashboard
+					</Link>
+				)}
 
-      <SignedIn>
-        <SignOutButton/>
-      </SignedIn>
+				<SignedOut>
+					<SignInOAuthButtons />
+				</SignedOut>
 
-
-     <SignedOut>
-      <SignInOAuthButtons/>
-     </SignedOut>
-
-     <UserButton/>
-      
-
-    </div>
-    
-    </div>  
-
-  )
-}
-
-export default TopBar
+				<UserButton />
+			</div>
+		</div>
+	);
+};
+export default Topbar;
